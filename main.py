7f02b18f26421ac0f3b7f0288de714adfddb1bbb
Dok4440 <dokimakimaki@gmail.com>
2022-11-13 06:34:40 -0500

    v1.4.3 changed the "playing" status if the bot is not defined as IS_MAIN in .env

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO)
 pr_client = discord.Bot(intents=discord.Intents.all())
+load_dotenv('.env')
 
 
 @pr_client.event
 async def on_ready():
     print('Bot is online.')
-    game = discord.Game("/help")
+
+    if os.getenv('ISMAIN') == "True":
+        game = discord.Game("/help")
+    else:
+        game = discord.Game("BETA")
+
     await pr_client.change_presence(status=discord.Status.do_not_disturb, activity=game)
 
 
@@ -19,5 +25,4 @@ for filename in os.listdir('./modules'):
     if filename.endswith('.py'):
         pr_client.load_extension(f'modules.{filename[:-3]}')
 
-load_dotenv('.env')
 pr_client.run(os.getenv('TOKEN'))
