741f2ba81d52f66122510c19f273560248411e69
Dok4440 <dokimakimaki@gmail.com>
2022-11-15 09:28:37 -0500

    v1.6.11 add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='praxem_rewrite',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='Dok4440',
+    author_email='',
+    description='A Discord bot.'
+)
